42cbfa1a3b0d4b9719b5cce0318706c181699e02
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2018-08-21 03:43:14 -0700

    Saves examples separately

--- words.py
+++ words.py
@@ -11,7 +11,7 @@ print("There are {} 2-gram pairs".format(len(ngramDB.word_pairs.keys())))
 
 ## Do some manual inspection with test words
 test_words = ["HELLO", "HOWDY", "FRIEND", "THANK", "EVERLASTING"]
-test_words = []
+# test_words = []
 for test_word in test_words:
 	print("\n###")
 	print(test_word)
@@ -21,21 +21,25 @@ for test_word in test_words:
 	nexts = ngramDB.find_next(test_word) 
 	print("5 Nexts", nexts[:min(5, len(nexts))])
 
-# START_WORD = "HELLO"
-NUM_SYLLABLES = 5
-RHYME_WORD = "MAY"
-DESTINATION_NODES = set([graph.WordNode(word, 0) for word in phoneticDB.find_rhymes(RHYME_WORD)])
-print(DESTINATION_NODES)
-word_graph = graph.TargetedGraph(phoneticDB, ngramDB, NUM_SYLLABLES, DESTINATION_NODES)
 
-SAVE_FREQ = 1000
-with open("examples/targeted.txt", "w") as fp:
-	poems = []
-	for word_list in word_graph.populate_graph():
-		poem = " ".join(word_list).lower()
-		poems.append(poem)
-		if len(poems) % SAVE_FREQ == 0:
-			print("Saving Poems")
-			fp.write("\n".join(poems))
-## General Graph Info
-print("There are {} vertices in the word graph".format(len(word_graph.vertices)))
+def test_graph(num_syllables, rhyme_word):
+	destination_nodes = set([graph.WordNode(word, 0) for word in phoneticDB.find_rhymes(rhyme_word)])
+	word_graph = graph.TargetedGraph(phoneticDB, ngramDB, num_syllables, destination_nodes)
+
+	SAVE_FREQ = 1000
+	with open("examples/targeted_{}.txt".format(rhyme_word), "w") as fp:
+		poems = []
+		for word_list in word_graph.populate_graph():
+			poem = " ".join(word_list).lower()
+			poems.append(poem)
+			if len(poems) % SAVE_FREQ == 0:
+				print("Saving Poems")
+				fp.write("\n".join(poems))
+	## General Graph Info
+	print("There are {} vertices in the word graph".format(len(word_graph.vertices)))
+
+test_graph(5, "CRUSH")
+test_graph(4, "FLY")
+test_graph(6, "FLOW")
+test_graph(5, "ANGEL")
+test_graph(5, "FIRE")
